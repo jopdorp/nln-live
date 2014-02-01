ebdb0aac638de4d9783150bc42ad000343830840
Jegor Opdorp <supradeus@hotmail.com>
2014-02-01 18:05:08 +0100

    Refactored PieceParser

--- api/services/PieceParser.js
+++ api/services/PieceParser.js
@@ -12,43 +12,18 @@ exports.parseDirectory = function (path) {
             graph: graph,
             metadata: metadata
         }
-    ).done(function (err, piece) {
-            if (err) {
-                return console.log(err, piece);
-            } else {
-                var instruments = walkInstruments(path);
-                for(var instrumentName in instruments ){
-                    var instrument = instruments[instrumentName];
-                    for(var i in instrument){
-                        var musicalBlock = instrument[i];
-                        musicalBlock.instrument = instrumentName;
-                        musicalBlock.pieceId = piece.id;
-
-                        MusicalBlock.create(musicalBlock).done(function(err2,block){
-                                if (err2) {
-                                    return console.log(err2, block);
-                                } else {
-                                    console.log("succesfully created musicalBlock",block);
-                                }
-                        });
-                    }
-                }
-                return console.log("succesfully created piece", piece);
-            }
+    ).done(function(err,piece){
+            return onPieceCreated(err,piece,path);
         }
     );
 };
 
 function walkInstruments(path) {
     var paths = [];
-    var walker = walk.walkSync(path, {
+    walk.walkSync(path, {
         followLinks: false,
         listeners: {
             directories: function (root, dirStatsArray, next) {
-                // dirStatsArray is an array of `stat` objects with the additional attributes
-                // * type
-                // * error
-                // * name
                 dirStatsArray.forEach(
                     function(dir) {
                         paths.push({path:root + "/" + dir.name,name:dir.name});
@@ -68,7 +43,7 @@ function walkInstruments(path) {
 
 function walkInstrumentDir(path) {
     var paths = [];
-    var walker = walk.walkSync(path, {
+    walk.walkSync(path, {
         followLinks: false,
         listeners: {
             file: function (root, fileStats, next) {
@@ -79,17 +54,53 @@ function walkInstrumentDir(path) {
     });
     var musicalBlocks = [];
     for (var i in paths) {
-        var block = {};
-        if (S(paths[i]).endsWith(".png")) {
-            block.imagePath = paths[i];
-        }
-        if (S(paths[i]).endsWith(".xml")) {
-            block.musicXml = fs.readFileSync(paths[i], 'utf8');
-        }
-        if (S(paths[i]).endsWith(".abc")) {
-            block.abc = fs.readFileSync(paths[i], 'utf8');
-        }
-         musicalBlocks.push(block);
+        musicalBlocks.push(
+            getBlockPaths(paths, i, musicalBlocks)
+        );
     }
     return musicalBlocks;
+}
+
+function getBlockPaths(path) {
+    var block = {};
+    if (S(path).endsWith(".png")) {
+        block.imagePath = path;
+    }
+    if (S(path).endsWith(".xml")) {
+        block.musicXml = fs.readFileSync(path, 'utf8');
+    }
+    if (S(path).endsWith(".abc")) {
+        block.abc = fs.readFileSync(path, 'utf8');
+    }
+    return block;
+}
+
+function onPieceCreated (err, piece,path) {
+    if (err) {
+        return console.log(err, piece);
+    } else {
+        var instruments = walkInstruments(path);
+        for(var instrumentName in instruments ) {
+            createMusicalBlock(instruments, instrumentName, piece);
+        }
+        return console.log("succesfully created piece", piece);
+    }
+}
+
+function createMusicalBlock(instruments, instrumentName, piece) {
+    var instrument = instruments[instrumentName];
+    for (var i in instrument) {
+        var musicalBlock = instrument[i];
+        musicalBlock.instrument = instrumentName;
+        musicalBlock.pieceId = piece.id;
+
+        MusicalBlock.create(musicalBlock).done(function (err2, block) {
+            if (err2) {
+                return console.log(err2, block);
+            } else {
+                console.log("succesfully created musicalBlock", block);
+            }
+        });
+    }
+    return musicalBlock;
 }
