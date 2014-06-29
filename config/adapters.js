8e07dbfdf80e96a56343e62dcf3edee9540240ee
Jegor van Opdorp <supradeus@hotmail.com>
2014-06-29 22:49:19 +0200

    added mozart and re added mongo support. default still is harddisk

--- config/adapters.js
+++ config/adapters.js
@@ -3,15 +3,12 @@ module.exports.adapters = {
 
     // sails v.0.9.0
     mongo: {
-        module   : 'sails-disk',
+        module   : 'sails-mongo',
         host     : 'localhost',
         port     : 27017,
         user     : '',
         password : '',
         database : 'nln-live',
-
-        // OR
-        module   : 'sails-mongo',
         url      : 'mongodb://localhost'
     },
 
