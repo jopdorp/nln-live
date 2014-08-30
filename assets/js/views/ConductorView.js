205a126cff7e094ff277d979fe090af2f05aa8b6
Jegor van Opdorp <supradeus@hotmail.com>
2014-08-30 16:19:45 +0300

    #49 Conductor doubleclick prevention leads to delays

--- assets/js/views/ConductorView.js
+++ assets/js/views/ConductorView.js
@@ -5,9 +5,13 @@ define([
     'text!../templates/conductor.hbs'
 ], function (Backbone, Handlebars, NotesDisplayView, conductorTemplate) {
     return  Backbone.View.extend({
+
         tagName: 'div',
         className: 'conductor',
         template: Handlebars.compile(conductorTemplate),
+
+        nextFragmentDelayed : false,
+
 
         events: {
             'click .fragment': 'onFragmentClick'
@@ -32,10 +36,16 @@ define([
             return this;
         },
 
-        onFragmentClick: _.debounce(function () {
-            console.log("about to emit /performance/conductNextFragment");
-            socket.get('/performance/conductNextFragment', {performanceId: this.options.performanceId});
-        },1000)
-
+        onFragmentClick: function(){
+            if(!this.nextFragmentDelayed){
+                console.log("about to emit /performance/conductNextFragment");
+                socket.get('/performance/conductNextFragment', {performanceId: this.options.performanceId});
+                this.nextFragmentDelayed = true;
+                var self = this;
+                setTimeout(function(){
+                    self.nextFragmentDelayed = false;
+                },1000)
+            }
+        }
     });
 });
